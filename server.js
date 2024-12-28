4bd15be6e2e2f7e648efcf88850498c42a1739b9
justinrivera382 <justinrivera382@proton.me>
2024-12-28 00:11:19 -0500

    Created basic routes and responses

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ dotenv.config({ path: "./config/config.env" });
 // initialize app
 const app = express();
 
+app.get("/api/v1/posts", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all posts" });
+});
+
+app.get("/api/v1/posts/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/posts", (req, res) => {
+  res.status(200).json({ success: true, msg: "Created new post" });
+});
+
+app.put("/api/v1/posts/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Updated post ${req.params.id}` });
+});
+
+app.delete("/api/v1/posts/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Deleted post ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 // set up server
